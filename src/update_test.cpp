518e80a98287b46ba22afb98ab2fcd979094e5fe
bluefeet32 <rshort@tcd.ie>
2012-10-04 19:38:46 +0100

    put some tests into update_test

--- src/update_test.cpp
+++ src/update_test.cpp
@@ -7,19 +7,45 @@
 
 int main()
 {
-	double timeStep, a, k, r;
-	board<tile> b(10, 10);
+	double timeStep, a, b, k, l, m, r, newHareSum, oldHareSum, newPumaSum, oldPumaSum;
+	board<tile> field(10, 10), old_field(10,10);
 
-	a = k = r = 0.0;
+	a = b = k = l = m = r = timeStep = 1.0;
 
 	for(size_t i=0; i<10; ++i)
 	for(size_t j=0; j<10; ++j)
 	{
-		b(i,j).hare = 1;
-		b(i,j).puma = 0;
+		field(i,j).hare = old_field(i,j).hare = 1;
+		field(i,j).puma = old_field(i,j).puma = 0;
 	}
 
-	update_hares(b, timeStep, a, k, r);
 	
+
+	update_hares(field, timeStep, a, k, r);
+	update_pumas(field, timeStep, b, l, m, r);	
+
+	newHareSum = oldHareSum = newPumaSum = oldPumaSum = 0;	
+
+	for(size_t i=0; i<10; ++i)
+	for(size_t j=0; j<10; ++j)
+	{
+		newHareSum += field(i,j).hare;
+		oldHareSum += old_field(i,j).hare;
+		newPumaSum += field(i,j).puma;
+		oldPumaSum += old_field(i,j).puma;
+	}
+	
+	if(newHareSum == oldHareSum)
+	{
+		cout << "error: hare sums not changing" << endl;
+		return 1;
+	}
+	else if(newPumaSum != 0)
+	{
+		cout << "error: pumas have spawned" << endl;
+		return 1;
+	}
+	
+
 	return 0;
 }
