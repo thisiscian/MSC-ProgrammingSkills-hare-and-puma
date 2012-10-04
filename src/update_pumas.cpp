7babe903bc704164ed34406080b1a0537a1c75c6
bluefeet32 <rshort@tcd.ie>
2012-10-04 19:21:31 +0100

    added update_pumas and edited makefile

---
+++ src/update_pumas.cpp
@@ -0,0 +1,25 @@
+#include "update.h"
+
+/*function that takes in the array of pumas and updates it using the update equations*/
+
+void update_pumas(board<tile> &field, double timeStep, double b, double l, double m, double r)
+{
+
+	int NY = field.get_height();
+	int NX = field.get_width();
+	board<tile> new_field(NX,NY);
+
+
+	for(int y=1; y<NY-1; y++)
+	for(int x=1; x<NX-1; x++)
+	{
+		new_field(x,y).puma = field(x,y).puma + timeStep* (b*field(x,y).hare*field(x,y).puma - m*field(x,y).puma + l*(field(x-1,y).puma + field(x+1,y).puma + field(x,y-1).puma + field(x,y+1).puma) - (field(x-1,y).is_land() + field(x+1,y).is_land() + field(x,y-1).is_land() + field(x,y+1).is_land())*field(x,y).puma);
+	}
+
+	for(int y=1; y<NY-1; y++)
+	for(int x=1; x<NX-1; x++)
+	{	
+			field(x,y).puma = new_field(x,y).puma;
+	}
+	
+}
