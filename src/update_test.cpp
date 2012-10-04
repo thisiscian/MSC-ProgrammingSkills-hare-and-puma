bab476b071617bc5d0855502496d177802982c3c
bluefeet32 <rshort@tcd.ie>
2012-10-04 23:24:28 +0100

    added puma death tests

--- src/update_test.cpp
+++ src/update_test.cpp
@@ -10,13 +10,17 @@ int main()
 	double timeStep, a, b, k, l, m, r, newHareSum, oldHareSum, newPumaSum, oldPumaSum;
 	board<tile> field(10, 10), old_field(10,10);
 
-	a = b = k = l = m = r = timeStep = 1.0;
+	a = b = k = l = m = r = timeStep = 1.0; // chosen so there are values
 
-	for(size_t i=0; i<10; ++i)
+//
+// initialise without pumas to test hare growth
+//
+
 	for(size_t j=0; j<10; ++j)
+	for(size_t i=0; i<10; ++i)
 	{
-		field(i,j).hare = old_field(i,j).hare = 1;
-		field(i,j).puma = old_field(i,j).puma = 0;
+		field(i,j).hare = old_field(i,j).hare = 1.0;
+		field(i,j).puma = old_field(i,j).puma = 0.0;
 	}
 
 	
@@ -26,8 +30,12 @@ int main()
 
 	newHareSum = oldHareSum = newPumaSum = oldPumaSum = 0;	
 
-	for(size_t i=0; i<10; ++i)
+//
+// first test if the hares numbers are increasing and pumas staying at 0
+//
+
 	for(size_t j=0; j<10; ++j)
+	for(size_t i=0; i<10; ++i)
 	{
 		newHareSum += field(i,j).hare;
 		oldHareSum += old_field(i,j).hare;
@@ -45,7 +53,52 @@ int main()
 		cout << "error: pumas have spawned" << endl;
 		return 1;
 	}
+
+//
+// reinitialise for re-testing, this time of puma death occuring
+// this requires a short loop over updates
+//
 	
+	for(size_t j=0; j<10; ++j)
+	for(size_t i=0; i<10; ++i)
+	{
+		field(i,j).hare = old_field(i,j).hare = 0.0;
+		field(i,j).puma = old_field(i,j).puma = 1.0;
+	}
+
+	for(int i=0;i<5;++i)
+	{
+		update_hares(field, timeStep, a, k, r);
+		update_pumas(field, timeStep, b, l, m, r);	
+	}
+
+
+//
+// next test if the pumas die if there are no hares and the hares don't spawn
+//
+
+	newHareSum = oldHareSum = newPumaSum = oldPumaSum = 0;	
+
+	for(size_t j=0; j<10; ++j)
+	for(size_t i=0; i<10; ++i)
+	{
+		newHareSum += field(i,j).hare;
+		oldHareSum += old_field(i,j).hare;
+		newPumaSum += field(i,j).puma;
+		oldPumaSum += old_field(i,j).puma;
+	}
+	
+	if(newHareSum != 0)
+	{
+		cout << "error: hares spawning" << endl;
+		return 1;
+	}
+	else if(newPumaSum >= oldPumaSum)
+	{
+		cout << "error: pumas not dying" << endl;
+		return 1;
+	}
+
 
 	return 0;
 }
