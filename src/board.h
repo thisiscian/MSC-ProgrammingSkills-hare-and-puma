e458765ed578feba5c837e1611311748e2b16d1e
Padraig O Conbhui <poconbhui@gmail.com>
2012-10-04 12:37:44 +0100

    Added board class

---
+++ src/board.h
@@ -0,0 +1,53 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <stdlib.h>
+
+template<class T>
+class board
+{
+  public:
+
+  board(size_t height_in, size_t width_in)
+  {
+    board_array = NULL;
+    resize_board(height_in, width_in);
+  }
+
+
+  // return board position (x_pos,y_pos)
+  T& operator()(size_t x_pos, size_t y_pos)
+  {
+    // have rightmost index contiguous in memory
+    //   as is standard in C
+    return board_array[x_pos*height + y_pos];
+  }
+
+
+  private:
+
+  // free old board, set height and width, allocate new board
+  void resize_board(size_t height_in, size_t width_in)
+  {
+    // free old board
+    delete[] board_array;
+
+    // set new sizes
+    height = height_in;
+    width  = width_in;
+
+    // make space for new board
+    board_array = new T[height*width];
+  }
+
+
+
+  size_t height;
+  size_t width;
+
+  T* board_array;
+
+};
+
+
+#endif
