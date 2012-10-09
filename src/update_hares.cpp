31e2a397adcf7715f678fcc83b983a34d537d52b
bluefeet32 <rshort@tcd.ie>
2012-10-09 21:26:37 +0100

    stopped hares and pumas updating on water

--- src/update_hares.cpp
+++ src/update_hares.cpp
@@ -11,13 +11,14 @@ void update_hares(Board<Tile> &field, double timeStep, double a, double k, doubl
 
 
 	for(int y=1; y<NY-1; y++)
+	for(int x=1; x<NX-1; x++)
 	{
-		for(int x=1; x<NX-1; x++)
+		if(field(x,y).is_land() == 1)
 		{
-		new_field(x,y).hare = field(x,y).hare + timeStep* \
-		(r*field(x,y).hare - a*field(x,y).hare * field(x,y).puma + k* \
-		(field(x-1,y).hare + field(x+1,y).hare + field(x,y-1).hare + field(x,y+1).hare - \
-		(field(x+1,y).is_land() + field(x-1,y).is_land() + field(x,y+1).is_land() + field(x,y-1).is_land())*field(x,y).hare));
+			new_field(x,y).hare = field(x,y).hare + timeStep* \
+			(r*field(x,y).hare - a*field(x,y).hare * field(x,y).puma + k* \
+			(field(x-1,y).hare + field(x+1,y).hare + field(x,y-1).hare + field(x,y+1).hare - \
+			(field(x+1,y).is_land() + field(x-1,y).is_land() + field(x,y+1).is_land() + field(x,y-1).is_land())*field(x,y).hare));
 		}
 	}
 
