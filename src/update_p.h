90e910cebf4ec37dec0daa6af57a1a4ad79f07fd
bluefeet32 <rshort@tcd.ie>
2012-10-11 18:09:54 +0100

    added new update_p.h file for performace tests

---
+++ src/update_p.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include "board.h"
+#include "tile.h"
+/*header file containing the update routines*/
+
+using namespace std;
+
+void update_animals(Board<Tile> &field, double timeStep, double a, double b, double k, double l, double m, double r);
+
+
+void update_animals_p(Board<Tile> &field, double timeStep, double a, double b, double k, double l, double m, double r);
