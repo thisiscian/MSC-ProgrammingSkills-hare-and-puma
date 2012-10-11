f264a3b53d436a0a7fc92544c238a380dba448b3
thisiscian <thisiscian@gmail.com>
2012-10-11 12:50:16 +0100

    this should be fixed now

--- src/test_field.h
+++ src/test_field.h
@@ -1,5 +1,11 @@
 #include "tile.h"
 #include "board.h"
-#include <algorithm>
 #include <cmath>
+#include <algorithm>
+
 using namespace std;
+
+void defineConstantField(Board<Tile>& field);
+void defineLinearField(Board<Tile>& field);
+void defineNonLinearField(Board<Tile>& field);
+
