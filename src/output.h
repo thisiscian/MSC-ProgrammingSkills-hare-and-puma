2095f9c4ee9f5a869db5ce9606e1e6594a596eb2
thisiscian <thisiscian@gmail.com>
2012-11-06 08:54:07 +0000

    sorry forgot to add output.h

--- src/output.h
+++ src/output.h
@@ -13,8 +13,8 @@
 #include "statistics.h"
 
 void output_to_console(Board<Tile> field, double time);
-void write_ppm(Board<Tile> field, int iteration);
-void write_adjustable_ppm(Board<Tile> field, int iteration, int tileSize, int borderWidth, std::string title);
+void write_ppm(Board<Tile> field, double time);
+void write_adjustable_ppm(Board<Tile> field, double time, int tileSize, int borderWidth, std::string title);
 void output_averages(Board<Tile> field, double time);
 
 #endif
