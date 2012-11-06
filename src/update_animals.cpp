3eaf8405f4380fa70c322c0a1e07919e84f4e8e0
Padraig O Conbhui <poconbhui@gmail.com>
2012-11-06 20:10:20 +0000

    Added options to makefiles to run "make performancetest"

--- src/update_animals.cpp
+++ src/update_animals.cpp
@@ -4,64 +4,63 @@
 
 void update_animals(Board<Tile> &board, double timeStep, double a, double b, double k, double l, double m, double r)
 {
-
-	int NY = board.get_height();
-	int NX = board.get_width();
-	int landSum;
-	Board<Tile> new_board(NX,NY);
+  int NY = board.get_height();
+  int NX = board.get_width();
+  int landSum;
+  Board<Tile> new_board(NX,NY);
 
 #pragma omp parallel for default(none) shared(board, timeStep, a, b, k, l, m, r, NX, NY, new_board) private(landSum)
-	for(int y=1; y<NY-1; ++y)
-	for(int x=1; x<NX-1; ++x)
-	{
-		if(board(x,y).is_land())
-		{
-		landSum = 0;
-			for(int i=x-1; i<=x+1; i+=2)
-			{
-				if(board(i,y).is_land())
-				{
-					landSum++;
-				}
-			}
-			for(int i=y-1; i<=y+1; i+=2)
-			{
-				if(board(x,i).is_land())
-				{
-					landSum++;
-				}	
-			}
+  for(int y=1; y<NY-1; ++y)
+  for(int x=1; x<NX-1; ++x)
+  {
+    if(board(x,y).is_land())
+    {
+    landSum = 0;
+      for(int i=x-1; i<=x+1; i+=2)
+      {
+        if(board(i,y).is_land())
+        {
+          landSum++;
+        }
+      }
+      for(int i=y-1; i<=y+1; i+=2)
+      {
+        if(board(x,i).is_land())
+        {
+          landSum++;
+        }  
+      }
 
-			new_board(x,y).hare = board(x,y).hare + timeStep* \
-			(r*board(x,y).hare - a*board(x,y).hare * board(x,y).puma + k* \
-			(board(x-1,y).hare + board(x+1,y).hare + board(x,y-1).hare + board(x,y+1).hare - \
-			(landSum)*board(x,y).hare));
+      new_board(x,y).hare = board(x,y).hare + timeStep* \
+      (r*board(x,y).hare - a*board(x,y).hare * board(x,y).puma + k* \
+      (board(x-1,y).hare + board(x+1,y).hare + board(x,y-1).hare + board(x,y+1).hare - \
+      (landSum)*board(x,y).hare));
 
-			new_board(x,y).puma = board(x,y).puma + timeStep* \
+      new_board(x,y).puma = board(x,y).puma + timeStep* \
       (b*board(x,y).hare*board(x,y).puma - m*board(x,y).puma + l* \
       ((board(x-1,y).puma + board(x+1,y).puma + board(x,y-1).puma + board(x,y+1).puma) - \
       (landSum)*board(x,y).puma));
-		}
-		else
-		{
-			new_board(x,y).hare = new_board(x,y).puma = 0;
-		}
-	}
+    }
+    else
+    {
+      new_board(x,y).hare = new_board(x,y).puma = 0;
+    }
+  }
 
 #pragma omp parallel for default(none) shared(NX, NY, new_board, board)
-	for(int y=1; y<NY-1; ++y)
-	for(int x=1; x<NX-1; ++x)
-	{
-		if(new_board(x,y).hare < 0)
-		{
-			board(x,y).hare = 0;
-		}	
-		else
-		{
-			board(x,y).hare = new_board(x,y).hare;
-		}
+  for(int y=1; y<NY-1; ++y)
+  for(int x=1; x<NX-1; ++x)
+  {
+    if(new_board(x,y).hare < 0)
+    {
+      board(x,y).hare = 0;
+    }  
+    else
+    {
+      board(x,y).hare = new_board(x,y).hare;
+    }
 
-		if(new_board(x,y).puma < 0) 
+    if(new_board(x,y).puma < 0) 
     { 
       board(x,y).puma = 0; 
     }  
@@ -70,7 +69,6 @@ void update_animals(Board<Tile> &board, double timeStep, double a, double b, dou
     board(x,y).puma = new_board(x,y).puma;
     }
 
-	}
-	
+  }
 
 }
--- src/update_performance_test.cpp
+++ src/update_performance_test.cpp
@@ -10,78 +10,78 @@ using namespace std;
 
 int main()
 {
-	double timeStep, a, b, k, l, m, r;
-	Board<Tile> board(1000, 1000);
+  double timeStep, a, b, k, l, m, r;
+  Board<Tile> board(1000, 1000);
 
-	double start, elapsedTime;
+  double start, elapsedTime;
 
-	a = b = k = l = m = r = timeStep = 1.0; // chosen so there are values
+  a = b = k = l = m = r = timeStep = 1.0; // chosen so there are values
 
 //
 // initialise without pumas to test hare growth also make boundaries 0
 //
 
-	for(size_t j=0; j<board.get_height(); ++j)
-	for(size_t i=0; i<board.get_width(); ++i)
-	{
-		if(j == 0 || i == 0 || j == board.get_height()-1 || i == board.get_height()-1)
-		{
-			board(i,j).make_water();
-			board(i,j).hare = 0.0;
-			board(i,j).puma = 0.0;
-		}
-		else
-		{
-			board(i,j).make_land();
-			board(i,j).hare = 1.0;
-			board(i,j).puma = 0.5;
-		}
-	}
-	
-	start = time(NULL);
+  for(size_t j=0; j<board.get_height(); ++j)
+  for(size_t i=0; i<board.get_width(); ++i)
+  {
+    if(j == 0 || i == 0 || j == board.get_height()-1 || i == board.get_height()-1)
+    {
+      board(i,j).make_water();
+      board(i,j).hare = 0.0;
+      board(i,j).puma = 0.0;
+    }
+    else
+    {
+      board(i,j).make_land();
+      board(i,j).hare = 1.0;
+      board(i,j).puma = 0.5;
+    }
+  }
+  
+  start = time(NULL);
 
-	for(int i=0; i<50; ++i)
-	{
-		update_animals_p(board, timeStep, a, b, k, l, m, r);
-	}
-	
-	elapsedTime = time(NULL) - start;
+  for(int i=0; i<50; ++i)
+  {
+    update_animals_p(board, timeStep, a, b, k, l, m, r);
+  }
+  
+  elapsedTime = time(NULL) - start;
 
-	cout << "time on multiple threads was: " << elapsedTime << "\n";
+  cout << "time on multiple threads was: " << elapsedTime << "\n";
 
 
 //
 // reinitialise so the updates do the exact same operations for most accurate timing
 //
 
-	for(size_t j=0; j<board.get_height(); ++j)
-	for(size_t i=0; i<board.get_width(); ++i)
-	{
-		if(j == 0 || i == 0 || j == board.get_height()-1 || i == board.get_height()-1)
-		{
-			board(i,j).make_water();
-			board(i,j).hare = 0.0;
-			board(i,j).puma = 0.0;
-		}
-		else
-		{
-			board(i,j).make_land();
-			board(i,j).hare = 1.0;
-			board(i,j).puma = 0.5;
-		}
-	}
-	
-	start = time(NULL);
+  for(size_t j=0; j<board.get_height(); ++j)
+  for(size_t i=0; i<board.get_width(); ++i)
+  {
+    if(j == 0 || i == 0 || j == board.get_height()-1 || i == board.get_height()-1)
+    {
+      board(i,j).make_water();
+      board(i,j).hare = 0.0;
+      board(i,j).puma = 0.0;
+    }
+    else
+    {
+      board(i,j).make_land();
+      board(i,j).hare = 1.0;
+      board(i,j).puma = 0.5;
+    }
+  }
+  
+  start = time(NULL);
 
-	for(int i=0; i<50; ++i)
-	{
-		update_animals(board, timeStep, a, b, k, l, m, r);
-	}
-	
-	elapsedTime = time(NULL) - start;
+  for(int i=0; i<50; ++i)
+  {
+    update_animals(board, timeStep, a, b, k, l, m, r);
+  }
+  
+  elapsedTime = time(NULL) - start;
 
-	cout << "time on one thread was: " << elapsedTime << "\n";
+  cout << "time on one thread was: " << elapsedTime << "\n";
 
-	return 0;
+  return 0;
 
 }
