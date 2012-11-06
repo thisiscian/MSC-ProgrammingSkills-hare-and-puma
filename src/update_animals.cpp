656967710320fb5904a315b01830617e92a892b1
bluefeet32 <rshort@tcd.ie>
2012-11-06 22:27:00 +0000

    moved hare and puma adjacent tile checking outside main computation and unrolled loop slightly update_perf 22secs 7.95455e+07Flops

--- src/update_animals.cpp
+++ src/update_animals.cpp
@@ -7,40 +7,52 @@ void update_animals(Board<Tile> &board, double timeStep, double a, double b, dou
 
 	int NY = board.get_height();
 	int NX = board.get_width();
-	int landSum;
+	int landSum, hareSum, pumaSum;
 	Board<Tile> new_board(NX,NY);
 
-#pragma omp parallel for default(none) shared(board, timeStep, a, b, k, l, m, r, NX, NY, new_board) private(landSum)
+#pragma omp parallel for default(none) shared(board, timeStep, a, b, k, l, m, r, NX, NY, new_board) private(landSum, hareSum, pumaSum) schedule(runtime)
 	for(int y=1; y<NY-1; ++y)
 	for(int x=1; x<NX-1; ++x)
 	{
 		if(board(x,y).is_land())
 		{
-		landSum = 0;
-			for(int i=x-1; i<=x+1; i+=2)
-			{
-				if(board(i,y).is_land())
-				{
-					landSum++;
-				}
-			}
-			for(int i=y-1; i<=y+1; i+=2)
-			{
-				if(board(x,i).is_land())
-				{
-					landSum++;
-				}	
-			}
+		landSum = hareSum = pumaSum = 0;
+
+      hareSum += board(x-1,y).hare;
+      pumaSum += board(x-1,y).puma;
+      if(board(x-1,y).is_land())
+      {
+        landSum++;
+      }
+
+      hareSum += board(x+1,y).hare;
+      pumaSum += board(x+1,y).puma;
+      if(board(x+1,y).is_land())
+      {
+        landSum++;
+      }
+
+      hareSum += board(x,y-1).hare;
+      pumaSum += board(x,y-1).puma;
+      if(board(x,y-1).is_land())
+      {
+        landSum++;
+      }
+
+      hareSum += board(x,y+1).hare;
+      pumaSum += board(x,y+1).puma;
+      if(board(x,y+1).is_land())
+      {
+        landSum++;
+      }
 
 			new_board(x,y).hare = board(x,y).hare + timeStep* \
 			(r*board(x,y).hare - a*board(x,y).hare * board(x,y).puma + k* \
-			(board(x-1,y).hare + board(x+1,y).hare + board(x,y-1).hare + board(x,y+1).hare - \
-			(landSum)*board(x,y).hare));
+			(hareSum - landSum*board(x,y).hare));
 
 			new_board(x,y).puma = board(x,y).puma + timeStep* \
       (b*board(x,y).hare*board(x,y).puma - m*board(x,y).puma + l* \
-      ((board(x-1,y).puma + board(x+1,y).puma + board(x,y-1).puma + board(x,y+1).puma) - \
-      (landSum)*board(x,y).puma));
+      (pumaSum - landSum*board(x,y).puma));
 		}
 		else
 		{
@@ -48,7 +60,7 @@ void update_animals(Board<Tile> &board, double timeStep, double a, double b, dou
 		}
 	}
 
-#pragma omp parallel for default(none) shared(NX, NY, new_board, board)
+#pragma omp parallel for default(none) shared(NX, NY, new_board, board) schedule(runtime)
 	for(int y=1; y<NY-1; ++y)
 	for(int x=1; x<NX-1; ++x)
 	{
